2ce0ca960341ea80e2843601e55568da2dc8f519
WilliamTatendaJose <40793621+WilliamTatendaJose@users.noreply.github.com>
2024-11-03 07:45:47 +0200

    sanity cred

--- src/app/shop/client.ts
+++ src/app/shop/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 const client = createClient({
-  projectId: 'your-project-id', // Replace with your Sanity project ID
+  projectId: 'o951bctg', // Replace with your Sanity project ID
   dataset: 'production',
   apiVersion: '2024-02-11',
   useCdn: true,
